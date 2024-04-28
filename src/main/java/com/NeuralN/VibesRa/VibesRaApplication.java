295b5ce895ff6d3b26b5246568e1a8ce647b7c8f
RicoMiles <ricoquiblat@gmail.com>
2024-04-29 02:46:50 +0800

    first commit

---
+++ src/main/java/com/NeuralN/VibesRa/VibesRaApplication.java
@@ -0,0 +1,13 @@
+package com.NeuralN.VibesRa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibesRaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VibesRaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/NeuralN/VibesRa/VibesRaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NeuralN.VibesRa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibesRaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
